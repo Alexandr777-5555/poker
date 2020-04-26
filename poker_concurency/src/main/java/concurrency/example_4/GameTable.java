1fa2c7c78e9e08590494a88ad4aa0999315922d9
Alexander <a-777-aaa@mail.ru>
2020-04-26 20:56:53 +0700

    concurrency v17

--- poker_concurency/src/main/java/concurrency/example_4/GameTable.java
+++ poker_concurency/src/main/java/concurrency/example_4/GameTable.java
@@ -1,7 +1,28 @@
 package concurrency.example_4;
 
+import concurrency.annotation.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+@ThreadSafe
 public class GameTable implements Table {
 
+    private List<Player> playerList;
+
+    public GameTable() {
+        playerList= Collections.synchronizedList(new ArrayList<Player>());
+    }
+
+    public void addPlayer(Player player){
+       synchronized (playerList){
+           if(!playerList.contains(player)){
+               playerList.add(player);
+           }
+       }
+    }
 
     @Override
     public Player startGame() {
