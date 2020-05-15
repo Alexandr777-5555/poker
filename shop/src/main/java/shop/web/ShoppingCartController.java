18dfcf7c8bc38597c1ce15ad9f161d91ec194e68
Alexander <a-777-aaa@mail.ru>
2020-05-15 12:38:17 +0700

    11

---
+++ shop/src/main/java/shop/web/ShoppingCartController.java
@@ -0,0 +1,42 @@
+package shop.web;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import shop.domain.ShoppingCart;
+import shop.service.ShoppingCartService;
+
+import java.util.Collections;
+import java.util.List;
+
+@Controller
+@RequestMapping("/shopCart")
+public class ShoppingCartController {
+
+    private final ShoppingCartService cartService;
+
+    public ShoppingCartController(ShoppingCartService cartService) {
+        this.cartService = cartService;
+    }
+
+
+    public void defaultForm() {
+
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String submitForm(@RequestParam("owner") String owner, Model model) {
+        List<ShoppingCart> cartList = Collections.emptyList();
+        if (owner != null) {
+            cartList = cartService.query(owner);
+        }
+        model.addAttribute("shopping", cartList);
+        return "shopCart";
+    }
+
+
+}
---
+++ shop/web/WEB-INF/jsp/shopcart.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: AVDEY
+  Date: 15.05.2020
+  Time: 12:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Shopping Cart</title>
+</head>
+<body>
+
+
+
+
+
+</body>
+</html>
