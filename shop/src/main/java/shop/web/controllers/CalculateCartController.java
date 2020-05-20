57518b65d850441269b6420e94de40f937cc50e0
Alexander <a-777-aaa@mail.ru>
2020-05-20 13:20:41 +0700

    25

--- shop/src/main/java/shop/web/controllers/CalculateCartController.java
+++ shop/src/main/java/shop/web/controllers/CalculateCartController.java
@@ -1,9 +1,17 @@
 package shop.web.controllers;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-
+@Controller
+@RequestMapping("/calculateCart")
 public class CalculateCartController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public void defaultForm() {
+    }
 
 
 }
