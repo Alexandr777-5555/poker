0f11bc3aa696262ac1c7fdd742548f6dfeb78ad4
Alexander <a-777-aaa@mail.ru>
2020-05-22 12:19:30 +0700

    31

--- shop/src/main/java/shop/web/controllers/CalculateCartController.java
+++ shop/src/main/java/shop/web/controllers/CalculateCartController.java
@@ -3,6 +3,7 @@ package shop.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +20,7 @@ public class CalculateCartController {
         this.cartService = cartService;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public void defaultForm() {
     }
 
