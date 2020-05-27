4abe472889a35ea887496eefa69c04727980826d
Alexander <a-777-aaa@mail.ru>
2020-05-27 21:28:06 +0700

    44

--- shop/src/main/java/shop/web/controllers/GlobalExceptionController.java
+++ shop/src/main/java/shop/web/controllers/GlobalExceptionController.java
@@ -1,6 +1,8 @@
 package shop.web.controllers;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -8,14 +10,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionController {
 
-
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 
     @ExceptionHandler(Exception.class)
     public void handleException() {
-
-
+        log.info("exeption");
     }
+
+
 
 
 }
