83438789990e7f1470f43168c087a1697ea340d8
Alexander <a-777-aaa@mail.ru>
2020-05-28 13:01:39 +0700

    45

--- shop/src/main/java/shop/web/controllers/GlobalExceptionController.java
+++ shop/src/main/java/shop/web/controllers/GlobalExceptionController.java
@@ -3,8 +3,11 @@ package shop.web.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @ControllerAdvice
@@ -13,12 +16,19 @@ public class GlobalExceptionController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 
-    @ExceptionHandler(Exception.class)
+    //@ResponseStatus(value = HttpStatus.NOT_FOUND)
+    // @ExceptionHandler(Exception.class)
     public void handleException() {
-        log.info("exeption");
+        // log.info("exeption");
     }
 
-
+    //@ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleAllException(Exception error) {
+        ModelAndView model = new ModelAndView("errors/error");
+        model.addObject("err" , error.getMessage());
+        return model;
+    }
 
 
 }
---
+++ shop/src/main/java/shop/web/controllers/RedirectController.java
@@ -0,0 +1,20 @@
+package shop.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RedirectController {
+
+
+    @RequestMapping(value = "/{type:.+}", method = RequestMethod.GET)
+    public ModelAndView getPages(@PathVariable("type") String type)
+            throws Exception {
+
+        throw new Exception();
+    }
+
+}
