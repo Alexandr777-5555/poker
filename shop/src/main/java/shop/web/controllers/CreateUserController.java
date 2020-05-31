21041b881728d135bc5b09a6968c1a4ee6c50f4f
Alexander <a-777-aaa@mail.ru>
2020-05-31 15:51:43 +0700

    51

--- shop/src/main/java/shop/web/controllers/CreateUserController.java
+++ shop/src/main/java/shop/web/controllers/CreateUserController.java
@@ -2,24 +2,26 @@ package shop.web.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import shop.domain.Customer;
 
 @Controller
 @RequestMapping("/createUser")
+@SessionAttributes("customer")
 public class CreateUserController {
 
 
     @GetMapping
-    public void setupForm() {
-
+    public void setupForm(Model model) {
+       Customer customer=new Customer();
+       model.addAttribute("customer" , customer);
     }
+
+
 
 
     @RequestMapping(method = RequestMethod.POST)
