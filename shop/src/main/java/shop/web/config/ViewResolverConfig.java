56d4bb92b63468b8b4c71c7aa59afd6269777b8b
Alexander <a-777-aaa@mail.ru>
2020-06-03 14:46:21 +0700

    63

--- shop/src/main/java/shop/web/config/ViewResolverConfig.java
+++ shop/src/main/java/shop/web/config/ViewResolverConfig.java
@@ -1,26 +1,48 @@
 package shop.web.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.ResourceBundleViewResolver;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
 public class ViewResolverConfig implements WebMvcConfigurer {
 
 
+    @Autowired
+    private ContentNegotiationManager manager;
 
 
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        Map<String, MediaType> mediatypes = new HashMap<>();
+        mediatypes.put("pdf", MediaType.valueOf("application/pdf"));
+        configurer.mediaTypes(mediatypes);
+    }
 
+    @Bean
+    public ContentNegotiatingViewResolver negotiatingViewResolver(){
+        ContentNegotiatingViewResolver viewResolver = new ContentNegotiatingViewResolver();
+        viewResolver.setContentNegotiationManager(manager);
+        return viewResolver;
+    }
 
 
     @Bean
     public ViewResolver pdfViewResolver() {
         ResourceBundleViewResolver viewResolver = new ResourceBundleViewResolver();
         viewResolver.setOrder(0);
-        viewResolver.setBasename("report");
+        viewResolver.setBasename("views");
         return viewResolver;
     }
 
