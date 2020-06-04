cc937c01a8cb9fadf48e3c8a370670be1827b26e
Alexander <a-777-aaa@mail.ru>
2020-06-04 16:42:14 +0700

    71

--- shop/src/main/java/shop/web/config/WebConfiguration.java
+++ shop/src/main/java/shop/web/config/WebConfiguration.java
@@ -4,12 +4,10 @@ package shop.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.sql.DataSource;
