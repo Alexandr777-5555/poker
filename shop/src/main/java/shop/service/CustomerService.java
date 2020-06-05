46e25ceaad8ecf34a025899293b8e03a6aeda8e8
Alexander <a-777-aaa@mail.ru>
2020-06-05 14:57:41 +0700

    73

---
+++ shop/src/main/java/shop/service/CustomerService.java
@@ -0,0 +1,10 @@
+package shop.service;
+
+
+import shop.domain.Customer;
+
+public interface CustomerService {
+
+    void save(Customer customer);
+
+}
---
+++ shop/src/main/java/shop/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,13 @@
+package shop.service.impl;
+
+import shop.domain.Customer;
+import shop.service.CustomerService;
+
+
+
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public void save(Customer customer) {
+
+    }
+}
