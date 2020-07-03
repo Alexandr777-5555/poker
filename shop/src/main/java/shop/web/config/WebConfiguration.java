8e0a7ec40f8e5c1d8d783b6ca79e0fa36dd84446
Alexander <a-777-aaa@mail.ru>
2020-07-03 16:31:05 +0700

    125

--- shop/src/main/java/shop/web/config/WebConfiguration.java
+++ shop/src/main/java/shop/web/config/WebConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan("shop")
-public class WebConfiguration {
+public class WebConfiguration  {
 
     @Bean
     public ViewResolver internalResourceViewResolver() {
