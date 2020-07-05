db3f920f4181aa09e59e332a0a12ce0e0803fe90
Alexander <a-777-aaa@mail.ru>
2020-07-05 14:12:49 +0700

    126

--- shop/src/main/java/shop/web/config/WebConfiguration.java
+++ shop/src/main/java/shop/web/config/WebConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 
 @Configuration
 @EnableWebMvc
@@ -21,6 +22,14 @@ public class WebConfiguration  {
         return viewResolver;
     }
 
+
+    @Bean
+    public TilesConfigurer tilesConfigurer() {
+        TilesConfigurer tilesConfigurer = new TilesConfigurer();
+        tilesConfigurer.setDefinitions(new String[] { "/WEB-INF/views/tiles/tiles.xml" });
+        tilesConfigurer.setCheckRefresh(true);
+        return tilesConfigurer;
+    }
 
 
 
