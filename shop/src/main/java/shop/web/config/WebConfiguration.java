7f930fbae2d92e28d8cb560af9f0f8605e794744
Alexander <a-777-aaa@mail.ru>
2020-07-06 13:05:36 +0700

    128

--- shop/src/main/java/shop/web/config/WebConfiguration.java
+++ shop/src/main/java/shop/web/config/WebConfiguration.java
@@ -4,9 +4,11 @@ package shop.web.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 
 @Configuration
@@ -19,8 +21,20 @@ public class WebConfiguration  {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
+        viewResolver.setOrder(0);
         return viewResolver;
     }
+
+    @Bean
+    public ViewResolver internalResourceViewResolverTiles() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/tiles/pages/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
+
 
 
     @Bean
---
+++ shop/web/WEB-INF/views/tiles/pages/home.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: AVDEY
+  Date: 06.07.2020
+  Time: 11:24
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+
+  Домашнаяя Apache Tiles
+
+
+</body>
+</html>
