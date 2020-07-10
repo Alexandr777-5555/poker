4308250ae48e49848b97e3aa8eb15689363058d8
Alexander <a-777-aaa@mail.ru>
2020-07-10 12:33:32 +0700

    138

---
+++ shop/src/main/java/shop/model/CustomerDiscountSummary.java
@@ -0,0 +1,19 @@
+package shop.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+
+
+@org.hibernate.annotations.Synchronize({"Customer", "Discount"})
+@Entity
+public class CustomerDiscountSummary {
+
+    @Id
+    private Long id;
+
+
+
+}
