b4c6556d47a7af5fd35ca1c530f4bc8518c3ff5c
Alexander <a-777-aaa@mail.ru>
2020-07-13 15:18:22 +0700

    142

--- shop/src/main/java/shop/repo/jpa/JpaCustomerRepo.java
+++ shop/src/main/java/shop/repo/jpa/JpaCustomerRepo.java
@@ -8,6 +8,7 @@ import shop.repo.CustomerRepository;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository("jpaCustomer")
@@ -33,15 +34,21 @@ public class JpaCustomerRepo implements CustomerRepository {
         } catch (RuntimeException ex) {
             transaction.rollback();
             throw ex;
-        }
-        finally {
+        } finally {
             manager.close();
         }
     }
 
     @Override
     public List<Customer> findAll() {
-        return null;
+        EntityManager manager = entityManagerFactory.createEntityManager();
+        try {
+            TypedQuery<Customer> query = manager
+                    .createQuery("select customer from Customer customer", Customer.class);
+            return query.getResultList();
+        } finally {
+            manager.close();
+        }
     }
 
     @Override
