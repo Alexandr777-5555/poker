a4814a3d5de62fc6e049ab2e8cc01d627d31db19
Alexander <a-777-aaa@mail.ru>
2020-07-13 11:25:12 +0700

    141

--- shop/src/main/java/shop/config/JpaConfig.java
+++ shop/src/main/java/shop/config/JpaConfig.java
@@ -1,7 +1,6 @@
 package shop.config;
 
 
-import org.hibernate.cfg.AvailableSettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -14,80 +13,24 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 public class JpaConfig {
 
-//    @Bean
-//    public CustomerRepository jpaCustomerRepository(EntityManagerFactory emf) {
-//        return new JpaCustomerRepo(emf);
-//    }
-
-//    @Bean
-//    public LocalEntityManagerFactoryBean emf() {
-//        LocalEntityManagerFactoryBean bean = new LocalEntityManagerFactoryBean();
-//        bean.setPersistenceUnitName("jparepo");
-//        bean.setDataSource(dataSource);
-//        bean.setJpaVendorAdapter(jpaVendorAdapter());
-//
-//        return bean;
-//    }
-
-//    @Bean
-//    public LocalContainerEntityManagerFactoryBean
-//    emf(DataSource dataSource) {
-//        LocalContainerEntityManagerFactoryBean emf =
-//                new LocalContainerEntityManagerFactoryBean();
-//      //  emf.setPersistenceUnitName("jparepo");
-//        emf.setDataSource(dataSource);
-//        emf.setJpaVendorAdapter(jpaVendorAdapter());
-//        return emf;
-//    }
-//
-//    private JpaVendorAdapter jpaVendorAdapter() {
-//        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
-//        jpaVendorAdapter.setShowSql(true);
-//        jpaVendorAdapter.setGenerateDdl(true);
-//        jpaVendorAdapter.setDatabasePlatform(PostgreSQL95Dialect.class.getName());
-//        return jpaVendorAdapter;
-//    }
-//
-//
-//    @Bean
-//    public DataSource dataSource() {
-//        HikariDataSource dataSource = new HikariDataSource();
-//       // dataSource.setUsername("postgres");
-//        dataSource.setUsername("");
-//        dataSource.setPassword("777");
-//        dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/shop");
-//        dataSource.setMinimumIdle(2);
-//        dataSource.setMaximumPoolSize(5);
-//        return dataSource;
-//    }
-//
-//
-//    @Bean
-//    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
-//        return new JpaTransactionManager(emf);
-//    }
-
 
     @Bean
-    public DataSource dataSource(){
+    public DataSource dataSource() {
         EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder()
                 .setType(EmbeddedDatabaseType.H2);//in-memory
-       // builder.addScript("schema.sql");
-       // builder.addScript("data.sql");
         return builder.build();
     }
-
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
         HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
         bean.setDatabase(org.springframework.orm.jpa.vendor.Database.H2);
         bean.setGenerateDdl(true);
+        bean.setShowSql(true);
         return bean;
     }
 
@@ -98,12 +41,6 @@ public class JpaConfig {
         bean.setDataSource(dataSource);
         bean.setJpaVendorAdapter(jpaVendorAdapter);
         bean.setPackagesToScan("shop");
-        Properties props = new Properties();
-        props.setProperty("dialect",
-                "org.hibernate.dialect.H2Dialect");
-        props.setProperty(AvailableSettings.SHOW_SQL, String.valueOf(true));
-        props.setProperty(AvailableSettings.HBM2DDL_AUTO, "update");
-        bean.setJpaProperties(props);
         return bean;
     }
 
