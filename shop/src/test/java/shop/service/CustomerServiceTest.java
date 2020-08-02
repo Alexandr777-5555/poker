598efb3ed053800564bad119e12e99e3d883cd5f
Alexander <a-777-aaa@mail.ru>
2020-08-02 14:33:19 +0700

    176

--- shop/src/test/java/shop/service/CustomerServiceTest.java
+++ shop/src/test/java/shop/service/CustomerServiceTest.java
@@ -1,7 +1,10 @@
 package shop.service;
 
+import org.junit.Before;
 import org.junit.Test;
 import shop.model.Customer;
+import shop.repo.memory.InMemoryCustomRepo;
+import shop.service.impl.CustomerServiceImpl;
 
 import static org.junit.Assert.*;
 
@@ -12,18 +15,29 @@ public class CustomerServiceTest {
     private Customer customer;
 
 
+    @Before
+    public void init() {
+        customer = new Customer();
+        customer.setId(1L);
+        service = new CustomerServiceImpl(new InMemoryCustomRepo());
+        service.create(customer);
+    }
 
 
     @Test
     public void create() {
+        assertEquals(customer, service.find(customer));
     }
 
     @Test
     public void remove() {
+        service.remove(customer);
+        assertEquals(0 , service.findAll().size());
     }
 
     @Test
-    public void find(){
-
+    public void find() {
+        Customer customer = service.find(this.customer);
+        assertEquals(customer, this.customer);
     }
 }
