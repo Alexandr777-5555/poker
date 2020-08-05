e1c8fbe174fbecd5ecf43db780e95d4a620b2cd9
Alexander <a-777-aaa@mail.ru>
2020-08-05 15:04:37 +0700

    181

--- shop/src/main/java/shop/config/JpaCustomerEmConfig.java
+++ shop/src/main/java/shop/config/JpaCustomerEmConfig.java
@@ -3,10 +3,12 @@ package shop.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -21,15 +23,18 @@ public class JpaCustomerEmConfig {
 
     @Bean
     public DataSource dataSource() {
-        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2);//in-memory
-        return builder.build();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(org.postgresql.Driver.class.getName());
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/shop");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("777");
+        return dataSource;
     }
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
         HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
-        bean.setDatabase(org.springframework.orm.jpa.vendor.Database.H2);
+        bean.setDatabase(Database.POSTGRESQL);
         bean.setGenerateDdl(true);
         bean.setShowSql(true);
         return bean;
