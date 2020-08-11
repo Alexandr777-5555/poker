2032d8a3b1539160677135ac884abf8517e29ab4
Alexander <a-777-aaa@mail.ru>
2020-08-11 13:36:26 +0700

    192

---
+++ shop/src/test/java/shop/config/TestConfig.java
@@ -0,0 +1,68 @@
+package shop.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import shop.repo.CustomerRepository;
+import shop.repo.jpa.JpaCustomerEmRepository;
+import shop.service.CustomerService;
+import shop.service.impl.CustomerServiceImpl;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan
+@Profile("test")
+public class TestConfig {
+
+
+    @Bean
+    public CustomerRepository repository() {
+        return new JpaCustomerEmRepository();
+    }
+
+
+    @Bean
+    public CustomerService service() {
+        return new CustomerServiceImpl(repository());
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(org.postgresql.Driver.class.getName());
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/shop");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("777");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
+        bean.setDatabase(Database.POSTGRESQL);
+        bean.setGenerateDdl(true);
+        bean.setShowSql(true);
+
+        return bean;
+    }
+
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+
+
+}
---
+++ shop/src/test/java/shop/service/impl/CustomerServiceImplTest.java
@@ -0,0 +1,39 @@
+package shop.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import shop.config.TestConfig;
+import shop.model.Customer;
+import shop.service.CustomerService;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@ActiveProfiles("test")
+public class CustomerServiceImplTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+
+    @Autowired
+    private CustomerService service;
+
+    @Test
+    public void findAll() {
+        List<Customer> all = service.findAll();
+        assertNotNull(all);
+        assertEquals(2, all.size());
+    }
+
+
+
+
+}
