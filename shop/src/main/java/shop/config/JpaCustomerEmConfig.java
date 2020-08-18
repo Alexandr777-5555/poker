58eb7c60acd86c74c7141f4fd7b711aef5622799
Alexander <a-777-aaa@mail.ru>
2020-08-18 12:09:18 +0700

    204

--- shop/src/main/java/shop/config/JpaCustomerEmConfig.java
+++ shop/src/main/java/shop/config/JpaCustomerEmConfig.java
@@ -1,48 +1,12 @@
 package shop.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.Database;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
 
 @Configuration
-@EnableTransactionManagement
 @Profile("jpaCustomerEmRepository")
+@Import(value = AppConfig.class)
 public class JpaCustomerEmConfig {
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(org.postgresql.Driver.class.getName());
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/shop");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("777");
-        return dataSource;
-    }
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
-        bean.setDatabase(Database.POSTGRESQL);
-        bean.setGenerateDdl(true);
-        bean.setShowSql(true);
-        return bean;
-    }
-
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-        return transactionManager;
-    }
 }
