b9aa8274eb81c1b245e218c5d0670f189c3c1717
Alexander <a-777-aaa@mail.ru>
2020-08-24 15:57:42 +0700

    214

---
+++ shop/src/main/java/shop/repo/ItemRepository.java
@@ -0,0 +1,23 @@
+package shop.repo;
+
+
+import shop.model.Item;
+
+import java.util.List;
+
+public interface ItemRepository {
+
+    void create(Item item);
+
+    void remove(Item item);
+
+    void remove(long id);
+
+    Item find(Item item);
+
+    boolean exists(Item item);
+
+    List<Item> findAll();
+
+    List<Item> findAllWithCost();
+}
