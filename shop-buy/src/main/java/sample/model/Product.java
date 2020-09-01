af91d0eb1db2fe432690e790d08fec5a5bf44a16
Alexander <a-777-aaa@mail.ru>
2020-09-01 14:19:07 +0700

    229

--- shop-buy/src/main/java/sample/model/Product.java
+++ shop-buy/src/main/java/sample/model/Product.java
@@ -1,9 +1,49 @@
 package sample.model;
 
 
-public class Product {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Product implements Serializable {
+
+    @Id
+    @Column(name = "PRODUCT_ID")
+    private String productId;
+
+    @Column(name = "PRICE")
+    private double price;
+
+    @ManyToMany
+    @JoinTable(name = "customer_product" ,
+            joinColumns = @JoinColumn(name = "PRODUCT_ID"),
+            inverseJoinColumns=@JoinColumn(name = "CUSTOMER_ID"))
+    private Set<Customer> customers=new HashSet<>();
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Set<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(Set<Customer> customers) {
+        this.customers = customers;
+    }
 
 
+    public double getPrice() {
+        return price;
+    }
 
-
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
