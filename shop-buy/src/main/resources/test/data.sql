90dae30973832efc7999c7590faf520ebbdf4439
Alexander <a-777-aaa@mail.ru>
2020-09-04 12:10:04 +0700

    235

---
+++ shop-buy/src/main/resources/test/data.sql
@@ -0,0 +1,3 @@
+ insert into customer (ID , BIRTH_DATE , FIRST_NAME , LAST_NAME ) values (1 , '1982-06-23' , 'Alex' , 'Avd');
+ insert into customer (ID , BIRTH_DATE , FIRST_NAME , LAST_NAME ) values (2 , '1987-02-02' , 'Natalia' , 'Ivanova');
+ insert into customer (ID , BIRTH_DATE , FIRST_NAME , LAST_NAME ) values (3 , '1981-03-03' , 'Светлана' , 'Potapova');
--- shop-buy/src/test/java/sample/service/impl/CustomerServiceImplTest.java
+++ shop-buy/src/test/java/sample/service/impl/CustomerServiceImplTest.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import sample.config.TestConfig;
@@ -30,11 +31,12 @@ public class CustomerServiceImplTest extends AbstractTransactionalJUnit4SpringCo
 
 
     @Test
+    @Sql({"classpath:test/data.sql" })
     public void findAll() {
-
         List<Customer> list=customerService.findAll();
-        assertEquals(0 , list.size());
+        assertEquals(3 , list.size());
     }
+
 
 
 }
