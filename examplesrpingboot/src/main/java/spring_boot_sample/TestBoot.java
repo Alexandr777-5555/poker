726647567946c67f6b9265e87269fce92a6b526a
Alexander <a-777-aaa@mail.ru>
2020-09-20 16:02:57 +0700

    253

--- examplesrpingboot/src/main/java/spring_boot_sample/TestBoot.java
+++ examplesrpingboot/src/main/java/spring_boot_sample/TestBoot.java
@@ -1,12 +1,14 @@
 package spring_boot_sample;
 
 
+import org.springframework.boot.SpringApplication;
+import spring_boot_sample.controller.NameController;
+
+
 public class TestBoot {
 
     public static void main(String[] args) {
-
-
-
-
+        SpringApplication.run(NameController.class , args);
     }
+
 }
--- examplesrpingboot/src/main/java/spring_boot_sample/controller/NameController.java
+++ examplesrpingboot/src/main/java/spring_boot_sample/controller/NameController.java
@@ -1,10 +1,20 @@
 package spring_boot_sample.controller;
 
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableAutoConfiguration
 public class NameController {
+
+
+    @GetMapping("/")
+    public String defaultForm(){
+        return "Hello Alexander";
+    }
+
 
 
 
